7cfc612cc8485358c93f0ba1ac972aa1c5298586
Shahfazlollahi <n_shahfazlollahi@tss-hr420-dm38.fc.ca>
2019-01-28 16:25:01 -0500

    new files

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,15 +1,37 @@
 (() => {
-	//select elements that you want to work with
-	let theButton = document.querySelector('#buttonHolder img');
-	//window.addEventListener("load",changeHeaderText);
+	//variable always comes first
+	// set up the puzzle pieces and boards
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	function changeHeaderText() {
-		document.querySelector('h1').textContent = "Tiny Meat Gang"
+	let piecesBoard = document.querySelector(".puzzle-pieces"),
+		puzzleBoard = document.querySelector(".puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+	//function goes in middle
+
+	function createPuzzlePieces(pictureIndex)
+	{
+		// generate puzzle pieces for left hand side
+		//debugger;
+		pieces.forEach((piece, index) => {
+		let newPuzzlePiece = `<img id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
+
+		piecesBoard.innerHTML += newPuzzlePiece;
+	});
+
+	puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`
+
 	}
 
-	//event handling goes at bottom
-	theButton.addEventListener("cick", changeHeaderText);
+	function resetPuzzlePieces() {
+		//empty thumbnail container
+		piecesBoard.innerHTML = "";
+		createPuzzlePieces(this.dataset.puzzleref)
+	}
+	//event handling at bottom
 	
-	// set up the puzzle pieces and boards
+	puzzleSelectors.forEach(puzzle => puzzle.addEventListener('click', resetPuzzlePieces));
+
+	createPuzzlePieces(0);
 
 })();
