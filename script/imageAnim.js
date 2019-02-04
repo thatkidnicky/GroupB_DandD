8a32d86938d5f628fd04dfcbfbbd4452ac1ba63c
Shahfazlollahi <n_shahfazlollahi@tss-hr420-dm38.fc.ca>
2019-02-04 16:36:38 -0500

    fix drag and drop

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -7,6 +7,8 @@
 		puzzleBoard = document.querySelector(".puzzle-board"),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+		let dropZones = document.querySelectorAll('.drop-zone');
+
 	//function goes in middle
 
 	function createPuzzlePieces(pictureIndex)
@@ -14,14 +16,46 @@
 		// generate puzzle pieces for left hand side
 		//debugger;
 		pieces.forEach((piece, index) => {
-		let newPuzzlePiece = `<img id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
+		let newPuzzlePiece = `<img draggable id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
 
 		piecesBoard.innerHTML += newPuzzlePiece;
 	});
 
 	puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`
 
+	initDrag();
+
 	}
+
+	//drag and drop goes here
+	
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img => {img.addEventListener("dragstart", function(e) {
+			//e.preventDefault();
+			console.log('draggin...')
+
+			e.dataTransfer.setData("text/plain", this.id);
+		});
+	});
+	}
+
+	// handle dragover and drop
+	// 
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log("Drag me Baby");
+		});
+
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log("Ouch my ASS")
+
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
+
 
 	function resetPuzzlePieces() {
 		//empty thumbnail container
